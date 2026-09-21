0d9551decf3bce27ababb093c5afc56029acbbff
Reinel <rjalfaro@unicesar.edu.co>
2026-09-21 15:56:31 -0500

    Configurar despliegue en Vercel con URL de API configurable

--- lib/services/api_config.dart
+++ lib/services/api_config.dart
@@ -1,3 +1,6 @@
 class ApiConfig {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  static const String baseUrl = String.fromEnvironment(
+    'API_BASE_URL',
+    defaultValue: 'http://127.0.0.1:8000',
+  );
 }
